f6ef3b7778f19ebe09edcbb634db18bbb3bcf5ee
Nicholas Ketchum <nketchum@gmail.com>
2017-03-26 15:34:27 -0400

    change from metatag advanced linkrel to an metatag opengraph meta property

--- src/Plugin/metatag/Tag/OgImageDefault.php
+++ src/Plugin/metatag/Tag/OgImageDefault.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\custom_metatag\Plugin\metatag\Tag;
 
-use \Drupal\metatag\Plugin\metatag\Tag\LinkRelBase;
+use \Drupal\metatag\Plugin\metatag\Tag\MetaPropertyBase;
 
 /**
  * Provides a plugin for the 'og:image:default' meta tag.
@@ -11,14 +11,14 @@ use \Drupal\metatag\Plugin\metatag\Tag\LinkRelBase;
  *   id = "og_image_default",
  *   label = @Translation("Default image"),
  *   description = @Translation("A default version of og:image defining a single image."),
- *   name = "image_default",
- *   group = "advanced",
+ *   name = "og:image:default",
+ *   group = "open_graph",
  *   weight = 10,
  *   type = "image",
  *   secure = FALSE,
  *   multiple = FALSE
  * )
  */
-class OgImageDefault extends LinkRelBase {
+class OgImageDefault extends MetaPropertyBase {
 
 }
